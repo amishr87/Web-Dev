92686b8b6eff8628af92bcb0017184a4d09747c3
amishr87 <amishr87@asu.edu>
2024-08-20 22:34:10 -0700

    commit

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,13 @@
+/*import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+*/
+
+import { randomSuperhero } from "superheroes";
+
+const name = randomSuperhero();
+
+console.log(`I am ${name}!`);
